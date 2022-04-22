e6fc351f46fcd830d2d4ae85d59e149f67db8678
RussiaFPS <nikitakrut58@gmail.com>
2022-04-22 20:52:25 +0300

    Добавление сортировки полей

--- pr3_3/app/src/main/java/ru/mirea/pr3_3/MainActivity.kt
+++ pr3_3/app/src/main/java/ru/mirea/pr3_3/MainActivity.kt
@@ -2,6 +2,7 @@ package ru.mirea.pr3_3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -20,9 +21,15 @@ class MainActivity : AppCompatActivity() {
         val jsonString = application.assets.open("input.json").bufferedReader().use{
             it.readText()
         }
+        var reverseSortByName:Boolean = false
+        var reverseSortBySex:Boolean = false
+        var reverseSortByPhone:Boolean = false
 
+        val sortByName: Button = findViewById(R.id.sortByName)
+        val sortBySex: Button = findViewById(R.id.sortBySex)
+        val sortByPhone: Button = findViewById(R.id.sortByPhone)
         val gson = GsonBuilder().create()
-        val Model= gson.fromJson(jsonString,Array<Person>::class.java).toList()
+        var Model= gson.fromJson(jsonString,Array<Person>::class.java).toList()
 
 
         val recyclerview = findViewById<RecyclerView>(R.id.recyclerview)
@@ -31,5 +38,45 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = CustomAdapter(Model)
         recyclerview.adapter = adapter
+
+
+        sortByName.setOnClickListener{
+            if (!reverseSortByName){
+                 Model = Model.sortedBy { it.name }
+                 reverseSortByName = true
+            }else{
+                Model = Model.sortedBy { it.name }.reversed()
+                reverseSortByName = false
+            }
+
+            val adapter = CustomAdapter(Model)
+            recyclerview.adapter = adapter
+        }
+
+        sortBySex.setOnClickListener{
+            if (!reverseSortBySex){
+                Model = Model.sortedBy { it.sex }
+                reverseSortBySex = true
+            }else{
+                Model = Model.sortedBy { it.sex }.reversed()
+                reverseSortBySex = false
+            }
+
+            val adapter = CustomAdapter(Model)
+            recyclerview.adapter = adapter
+        }
+
+        sortByPhone.setOnClickListener{
+            if (!reverseSortByPhone){
+                Model = Model.sortedBy { it.phoneNumber }
+                reverseSortByPhone = true
+            }else{
+                Model = Model.sortedBy { it.phoneNumber }.reversed()
+                reverseSortByPhone = false
+            }
+
+            val adapter = CustomAdapter(Model)
+            recyclerview.adapter = adapter
+        }
     }
 }
