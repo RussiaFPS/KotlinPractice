3e0288eea3a3a90d8ee2bb2e9a7b5aa0a9a828b7
RussiaFPS <nikitakrut58@gmail.com>
2022-04-28 11:54:59 +0300

    Сохранение состояния при повороте в 3_5

--- pr3_5/app/src/main/java/ru/mirea/pr3_5/MainActivity.kt
+++ pr3_5/app/src/main/java/ru/mirea/pr3_5/MainActivity.kt
@@ -1,29 +1,47 @@
 package ru.mirea.pr3_5
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+    private var check:Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button: Button = findViewById(R.id.caption)
-        var check:Boolean = false
+        check = savedInstanceState?.getBoolean("status") ?: false
 
-        val defFragment1: BlankFragmentRed = BlankFragmentRed.newInstance()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment1, defFragment1)
-            .commit()
+        if (!check) {
 
-        val defFragment2: BlankFragmentBlue = BlankFragmentBlue.newInstance()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragment2, defFragment2)
-            .commit()
+            val defFragment1: BlankFragmentRed = BlankFragmentRed.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment1, defFragment1)
+                .commit()
+
+            val defFragment2: BlankFragmentBlue = BlankFragmentBlue.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment2, defFragment2)
+                .commit()
+        }else{
+            val defFragment1: BlankFragmentRed = BlankFragmentRed.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment2, defFragment1)
+                .commit()
+
+            val defFragment2: BlankFragmentBlue = BlankFragmentBlue.newInstance()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment1, defFragment2)
+                .commit()
+        }
 
         button.setOnClickListener {
             val fragmentManager = supportFragmentManager
@@ -42,4 +60,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("status", check)
+    }
 }
